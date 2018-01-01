0bcbcf1ee8021ddf5fc0a744b6a389700f78dfcf
Pavel Ermakov <pavermakov@PavelErakovsMBP.Dlink>
2018-01-01 20:05:51 +0300

    initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './App.less';
+
+const App = () => (
+  <div className="app">
+    Welcome to React Boilerplate
+  </div>
+);
+
+export default App;
